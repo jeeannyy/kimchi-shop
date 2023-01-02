d132b14ae0c389e946687bfafc1641836e754301
Jeeann <jeeann.kim@gmail.com>
2023-01-03 02:08:57 +0900

    add kimchi img

--- src/data.js
+++ src/data.js
@@ -1,23 +1,86 @@
 let data = [
     {
       id : 0,
-      title : "White and Black",
-      content : "Born in France",
-      price : 120
+      title : "Pa Kimchi",
+      content : "Green onion kimchi",
+      price : 30
     },
   
     {
       id : 1,
-      title : "Red Knit",
-      content : "Born in Seoul",
-      price : 110
+      title : "Yeolmu-kimchi",
+      content : "Young radish stalks kimchi",
+      price : 30
     },
   
     {
       id : 2,
-      title : "Grey Yordan",
-      content : "Born in the States",
-      price : 130
+      title : "Kkakdugi",
+      content : "Sliced[cubed] radish kimchi",
+      price : 30
+    },
+
+    {
+      id : 3,
+      title : "Dongchimi",
+      content : "Radish water kimchi",
+      price : 30
+    },
+
+    {
+      id : 4,
+      title : "Baechu kimchi",
+      content : "Cabbage Kimchi",
+      price : 30
+    },
+
+    {
+      id : 5,
+      title : "Perilla leaf kimchi",
+      content : "Kkaennip kimchi",
+      price : 30
+    },
+
+    {
+      id : 6,
+      title : "Musaengchae",
+      content : "Shredded white radish",
+      price : 30
+    },
+
+    {
+      id : 7,
+      title : "Baek kimchi",
+      content : "White Cabbage Kimchi",
+      price : 30
+    },
+
+    {
+      id : 8,
+      title : "Seasoned Sweet Potato Stalks kimchi",
+      content : "Gogumasun kimchi",
+      price : 30
+    },
+
+    {
+      id : 9,
+      title : "Leek kimchi",
+      content : "Buchu kimchi",
+      price : 30
+    },
+
+    {
+      id : 10,
+      title : "Oisobagi",
+      content : "Cucumber pickles",
+      price : 30
+    },
+
+    {
+      id : 11,
+      title : "Young radish kimchi",
+      content : "Chonggak kimchi",
+      price : 30
     }
   ] 
 
